7bfe224d402b5c619a4856d235eab8fb5ea55e55
ayanasunakawa <ayana.sunakawa@di-v.co.jp>
2021-08-23 19:12:06 +0900

    Add usernamecheck

--- check.rb
+++ check.rb
@@ -30,13 +30,13 @@ random_title = SecureRandom.alphanumeric(6)
 @prototype_catch_copy2 = "色の迫力"
 @prototype_concept = "感情と色"
 @prototype_image_name = "sakura.jpeg"
-@prototype_image = "/Users/tech-camp/protospace_check/protospace_check/sakura.jpeg"
+@prototype_image = "/Users/tech-camp/development/protospace_check/sakura.jpeg"
 @comment = "素晴らしい！"
 
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument('--headless')
 @d = Selenium::WebDriver.for :chrome
-@d = Selenium::WebDriver.for :chrome , options: options
+# @d = Selenium::WebDriver.for :chrome , options: options
 @wait = Selenium::WebDriver::Wait.new(:timeout => 80)
 
 # チェック項目の結果や詳細を保存する配列
